da75945aef98ba7bfc6c186b0c0ca4b2d1df7110
Diana Pulatova <dianapulatovaa@gmail.com>
2020-07-27 21:02:39 -0400

    renewed profile pic

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
+
+  // for the form
+
+  $(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+  });
+      
         
