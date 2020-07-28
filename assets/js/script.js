1b168ab6babb165d0f1017824883a7a0b0c29345
Diana Pulatova <dianapulatovaa@gmail.com>
2020-07-27 20:42:22 -0400

    added extra sudo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,13 @@
 //     $('.parallax').parallax();
 //   });
 
+// parallax for pictures
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.parallax');
   var instances = M.Parallax.init(elems);
 });
 
+// for drop down menu
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
